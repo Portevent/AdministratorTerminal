a2c17b4a208b6355e7a82f5de5c3c6df6c6269e0
Portevent <74187611+Portevent@users.noreply.github.com>
2025-01-13 10:01:31 +0100

    feat : moved into flight_deck

---
+++ flight_deck/client/client.py
@@ -0,0 +1,28 @@
+import curses
+from abc import abstractmethod
+
+from display.display import Display
+
+
+class Client:
+    """
+    Client that has a display a windows and listen its input
+    """
+    display: Display
+
+    def __init__(self, display: Display):
+        self.display = display
+
+    @abstractmethod
+    def onkey(self, key: str):
+        raise NotImplementedError
+
+    def start(self):
+        self.display.start_listening(self.onkey)
+
+    def __enter__(self):
+        self.display.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return self.display.__exit__(exc_type, exc_val, exc_tb)
