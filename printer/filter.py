fdfd74adb1bd153614fec12016a19cdb0f7f4c4b
Aarys Sahler <aarys.sahler@gmail.com>
2025-02-04 12:19:58 +0100

    ugh

--- printer/filter.py
+++ printer/filter.py
@@ -1,9 +1,9 @@
 from forms.form import Form
-from forms.report_form import IncidentReportForm
 from printer.templates.mission_template import MissionTemplate
 from printer.templates.report_template import IncidentReportTemplate
 from printer.templates.template import Template
 
+from forms.report_form import IncidentReportForm
 from forms.mission_form import MissionForm
 
 class Filter:
@@ -11,8 +11,8 @@ class Filter:
     def templateFromDocument(document: Form) -> Template:
         match document:
             case MissionForm():
-                return MissionTemplate(document)
+                return MissionTemplate[MissionForm](document)
             case IncidentReportForm():
-                return IncidentReportTemplate(document)
+                return IncidentReportTemplate[IncidentReportForm](document)
             case _:
                 raise NotImplementedError
