0b40be2182a801fa7ed5b28fd510a0ed3067ce4c
Aarys Sahler <aarys.sahler@gmail.com>
2025-02-04 12:15:08 +0100

    Added IncidentReportTemplate to filter

--- printer/filter.py
+++ printer/filter.py
@@ -1,5 +1,7 @@
 from forms.form import Form
+from forms.report_form import IncidentReportForm
 from printer.templates.mission_template import MissionTemplate
+from printer.templates.report_template import IncidentReportTemplate
 from printer.templates.template import Template
 
 from forms.mission_form import MissionForm
@@ -10,5 +12,7 @@ class Filter:
         match document:
             case MissionForm():
                 return MissionTemplate(document)
+            case IncidentReportForm():
+                return IncidentReportTemplate(document)
             case _:
                 raise NotImplementedError
