3c8821ae3844396d4f93d85f322232b55ed006e4
Aarys Sahler <aarys.sahler@gmail.com>
2025-02-10 15:54:13 +0100

    fixed issues with Entity usage + updated template barcode to answer #8

--- printer/templates/mission_template.py
+++ printer/templates/mission_template.py
@@ -9,7 +9,7 @@ from forms.mission_form import MissionForm
 class MissionTemplate[T: MissionForm](Template[T]):
     document: T
 
-    def _print(self, printer: ThermalPrinter) -> None:
+    def _print(self, printer: ThermalPrinter, oid: int | None = None) -> None:
         """
         Prints the Mission
         """
@@ -26,7 +26,7 @@ class MissionTemplate[T: MissionForm](Template[T]):
 
         DefaultFormat.print_short_field(printer, "Full source ID", "undefined", end="\n")
         DefaultFormat.print_double_field(printer, "Status", "undef", "Dpt", "undef", end="\n\n")
-        DefaultFormat.print_short_field(printer, "Full dest ID", self.document.assigned_id, end="\n\n\n")
+        DefaultFormat.print_short_field(printer, "Full dest ID", self.document.assigned.getID(), end="\n\n\n")
 
         DefaultFormat.print_long_field(printer, "Service Object", self.document.name, end="\n")
         DefaultFormat.print_long_field(printer, "Location", self.document.location, end="\n\n\n")
@@ -36,9 +36,10 @@ class MissionTemplate[T: MissionForm](Template[T]):
         printer.set_with_default(bold=False)
         printer.text(" [ ] Read   [ ] In progress   [ ] Done\n\n\n")
 
+        if oid is None:
+            printer.text("OrderID propagation failed, barcode incomplete\n")
 
-        printer.text("need to change barcode code xxxxxxxx\n")
-        DefaultFormat.print_pdf417(printer, "oid.17649,s.PPORTE-E-0972D,d.STAR-L6904")
+        DefaultFormat.print_pdf417(printer, f"oid.{str(oid)},s.NOTSUPP,d.{self.document.assigned.getID()}")
         printer.set_with_default(bold=True)
 
         # printer.text(custom_center("fin de transmission"))
--- printer/templates/report_template.py
+++ printer/templates/report_template.py
@@ -9,7 +9,7 @@ from forms.report_form import IncidentReportForm
 class IncidentReportTemplate[T: IncidentReportForm](Template[T]):
     document: T
 
-    def _print(self, printer: ThermalPrinter) -> None:
+    def _print(self, printer: ThermalPrinter, oid: int | None = None) -> None:
         """
         Prints the Mission
         """
@@ -36,9 +36,10 @@ class IncidentReportTemplate[T: IncidentReportForm](Template[T]):
         if self.document.proposal is not None:
             DefaultFormat.print_long_field(printer, "Proposal", self.document.proposal, end="\n\n\n")
 
+        if oid is None:
+            printer.text("OrderID propagation failed, barcode incomplete\n")
 
-        printer.text("need to change barcode code xxxxxxxx\n")
-        DefaultFormat.print_pdf417(printer, "oid.17649,s.PPORTE-E-0972D,d.STAR-L6904")
+        DefaultFormat.print_pdf417(printer, f"oid.{str(oid)},s.{self.document.source.getID()},d.{self.document.dest.getID()}")
         printer.set_with_default(bold=True)
 
         # printer.text(custom_center("fin de transmission"))
